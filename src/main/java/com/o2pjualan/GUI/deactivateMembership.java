13618769d227629d92bf249de8e33c543e4569a0
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-04-22 22:22:56 +0700

    add gui member + animation

--- src/main/java/com/o2pjualan/GUI/deactivateMembership.java
+++ src/main/java/com/o2pjualan/GUI/deactivateMembership.java
@@ -1,4 +1,60 @@
 package com.o2pjualan.GUI;
 
-public class deactivateMembership {
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+public class deactivateMembership extends Tab {
+    private ComboBox<String> name;
+    private Button deactivate;
+    public deactivateMembership(){
+        this.setText("Deactivate Membership");
+
+        this.name = new ComboBox<>();
+        this.name.setId("nameDropDown");
+        this.name.setPromptText("Pick Customer Name...");
+
+        this.deactivate = new Button("Deactivate");
+
+        Label vipMembership = new Label("ARE YOU SURE YOU WANT TO DEACTIVATE?");
+        vipMembership.setId("h1");
+
+        Label info = new Label("Deactivating your account may lose the experience to various discounts.");
+        info.setId("paragraph");
+
+        VBox labelLayout = new VBox();
+        labelLayout.getChildren().addAll(vipMembership, info);
+        labelLayout.setSpacing(20);
+        labelLayout.setPadding(new Insets(20));
+        labelLayout.setAlignment(Pos.CENTER);
+
+        VBox upgradeLayout = new VBox();
+        upgradeLayout.getChildren().addAll(this.name, this.deactivate);
+        upgradeLayout.setSpacing(50);
+        upgradeLayout.setPadding(new Insets(20));
+        upgradeLayout.setAlignment(Pos.CENTER);
+
+        VBox wholeLayout = new VBox();
+        wholeLayout.getChildren().addAll(labelLayout, upgradeLayout);
+        wholeLayout.setAlignment(Pos.CENTER);
+        wholeLayout.setId("wholeLayout");
+        wholeLayout.setSpacing(100);
+        Pane base = new Pane();
+        base.getChildren().add(wholeLayout);
+        this.setContent(base);
+        wholeLayout.setFillWidth(true);
+        wholeLayout.prefWidthProperty().bind(base.widthProperty());
+        wholeLayout.prefHeightProperty().bind(base.heightProperty());
+        Image backgroundImage = new Image("file:src/img/deactivate.png");
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
+        base.setBackground(new Background(new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize)));
+
+    
+    }
 }
--- src/main/java/com/o2pjualan/GUI/history.java
+++ src/main/java/com/o2pjualan/GUI/history.java
@@ -1,4 +1,69 @@
 package com.o2pjualan.GUI;
 
-public class history {
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+public class history extends Tab {
+    public ComboBox<String> name;
+    public Label idTransaction;
+    public Label firstItem;
+    public Label totalPrice;
+
+    public ArrayList<Label> listOfBill;
+    public history(){
+
+        this.setText("History");
+        Label historyTitle = new Label("History");
+        historyTitle.setId("h1");
+
+        this.name = new ComboBox<>();
+        this.name.setPromptText("Pick Customer Name...");
+        this.name.setId("nameDropDown");
+
+        HBox upperLayout = new HBox();
+        upperLayout.getChildren().addAll(historyTitle, this.name);
+        upperLayout.setSpacing(900);
+
+        this.idTransaction = new Label("194129471");
+        this.idTransaction.setId("historyItemLabel");
+        this.firstItem = new Label("Tabung Elemenyer...");
+        this.firstItem.setId("historyItemLabel");
+        this.totalPrice = new Label("Total Pesanan: 1024124091");
+        this.totalPrice.setId("historyItemLabel");
+
+        VBox historyItemLayout = new VBox();
+        historyItemLayout.setId("historyItemLayout");
+        historyItemLayout.getChildren().addAll(this.idTransaction, this.firstItem, this.totalPrice);
+
+        VBox historyLayout = new VBox();
+        historyLayout.getChildren().addAll(historyItemLayout);
+        historyLayout.setSpacing(10);
+
+
+        VBox wholeLayout = new VBox();
+        wholeLayout.getChildren().addAll(upperLayout, historyLayout);
+        wholeLayout.setMargin(upperLayout, new Insets(10,0,0,0));
+        wholeLayout.setMargin(historyLayout, new Insets(10, 0, 0, 0));
+        wholeLayout.setSpacing(40);
+        wholeLayout.setId("wholeLayout");
+
+        Pane base = new Pane();
+        base.getChildren().add(wholeLayout);
+        this.setContent(base);
+        wholeLayout.setFillWidth(true);
+        wholeLayout.prefWidthProperty().bind(base.widthProperty());
+        wholeLayout.prefHeightProperty().bind(base.heightProperty());
+        wholeLayout.getStylesheets().add("file:src/main/java/com/o2pjualan/style/style.css");
+
+
+
+    }
 }
