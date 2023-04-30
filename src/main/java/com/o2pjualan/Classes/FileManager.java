6c99820048bb5cb5e99cf8f06f24204ac74a7647
Febryan Arota <13521120@mahasiswa.itb.ac.id>
2023-04-30 19:51:35 +0700

    add FileManager

---
+++ src/main/java/com/o2pjualan/Classes/FileManager.java
@@ -0,0 +1,136 @@
+package com.o2pjualan.Classes;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static com.o2pjualan.Main.folderName;
+import static com.o2pjualan.Main.controller;
+
+public class FileManager {
+    public static void initiateDataStore() throws IOException {
+        String[] fileNames = {"customer.xml", "customer.json", "customer.ser"};
+        Boolean found = false;
+        for (String fileName : fileNames) {
+            File file = new File(folderName + fileName);
+            if (file.exists()) {
+                String ext = fileName.substring( fileName.lastIndexOf(".") + 1);
+                controller = new Controller(ext);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            setUpFolder("json");
+            controller = new Controller("json");
+        }
+    }
+
+    public static void deleteCurrentFiles() {
+        String folderPath = folderName.substring(0, folderName.length()-1);
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+
+        if (files != null) {
+            for (File f: files) {
+                if (f.isFile()) {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+    public static boolean isExtExist(String ext) {
+        String folderPath = folderName.substring(0, folderName.length()-1);
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+        return(ext.equals(files[0].getName().substring(files[0].getName().lastIndexOf(".") + 1)));
+    }
+
+    public static void setUpFolder(String ext) {
+        deleteCurrentFiles();
+
+        if (ext == "obj") {
+            ext = "ser";
+        }
+
+        String[] newFileNames = {"customer.", "bill.", "fixedBill.", "product."};
+        for (int i = 0; i < newFileNames.length; i++) {
+            newFileNames[i] = newFileNames[i].concat(ext);
+        }
+        String bill = "";
+        String customer = "";
+        String fixedBill = "";
+        String product = "";
+
+        if (ext.equals("json")) {
+            bill = "[ ]";
+            customer = "[ ]";
+            fixedBill = "[ ]";
+            product = "[ ]";
+        } else if  (ext.equals("xml")) {
+            customer = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                    "<customers></customers>";
+            bill = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                    "<bills></bills>";
+            fixedBill = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                    "<fixedBills></fixedBills>";
+            product = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                    "<products></products>";
+        }
+
+        for (String fileName : newFileNames) {
+            File file = new File(folderName + fileName);
+            try {
+                file.createNewFile();
+                FileWriter writer = new FileWriter(folderName + fileName);
+                String content = fileName.substring(0, fileName.indexOf('.'));
+                System.out.println(content);
+                if (content.equals("customer") ) {
+                    writer.write(customer);
+                    writer.close();
+                } else if ( content.equals("bill")) {
+                    writer.write(bill);
+                    writer.close();
+                } else if ( content.equals("product")) {
+                    writer.write(product);
+                    writer.close();
+                } else if ( content.equals("fixedBill")) {
+                    writer.write(fixedBill);
+                    writer.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    //retcode 0 = success, 1 = already true, -1 = fail
+    public static Integer changeExt(String ext) {
+        Integer retcode = -1;
+        if (!isExtExist(ext)) {
+            Customers tempCustomers = controller.getCustomers();
+            Products tempProducts = controller.getProducts();
+            FixedBills tempFixedBills = controller.getFixedBills();
+            Bills tempBills = controller.getBills();
+
+            if (tempCustomers != null && tempBills != null && tempFixedBills != null && tempProducts != null) {
+                setUpFolder(ext);
+                try {
+                    controller = new Controller(ext);
+                    controller.saveDataCustomer(tempCustomers);
+                    controller.saveDataProduct(tempProducts);
+                    controller.saveDataFixedBill(tempFixedBills);
+                    controller.saveDataBill(tempBills);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                retcode = 0;
+            }
+        } else {
+            retcode = 1;
+        }
+
+        return retcode;
+    }
+}
